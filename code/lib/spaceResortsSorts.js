0a4ea4f63faf08488ba37fcb237ab77dd2a43778
Sarah Moliner-Roy <s.moliner@samsung.com>
2018-08-30 16:21:59 -0700

    Dialogs and training

--- code/lib/spaceResortsSorts.js
+++ code/lib/spaceResortsSorts.js
@@ -1,10 +1,8 @@
 module.exports = {
   "GravityASC": function(a, b) {
-    console.log("ASC", a.gravity - b.gravity)
     return a.gravity - b.gravity
   },
   "GravityDESC": function (a, b) {
-    console.log("DESC", b.gravity - a.gravity)
     return b.gravity - a.gravity
   }
 }
