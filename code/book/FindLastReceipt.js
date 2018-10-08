9d1a4103f3bce3b0e8333a9d5c2cc28265bde99d
Siamak Hodjat <shodjat@viv.ai>
2018-10-08 16:39:03 -0700

    use correct name space

--- code/book/FindLastReceipt.js
+++ code/book/FindLastReceipt.js
@@ -1,5 +1,5 @@
 // FindLastReceipt
 exports.function = function() {
     //look up last receipt from viv cloud
-    return transaction.retrieve("bixby.spaceResorts.Receipt", "ALL", 1)
+    return transaction.retrieve("example.spaceResorts.Receipt", "ALL", 1)
 }
