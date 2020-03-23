72bfb61ea07603c6262887cbfdf0fe37f1cb574f
kangawoo44 <wkang@viv.ai>
2020-03-23 12:38:42 -0700

    migrate ko training to adopt SpaceResort#all

--- code/find/FindSpaceResorts.js
+++ code/find/FindSpaceResorts.js
@@ -2,6 +2,6 @@ const main = require("./lib/main.js")
 
 module.exports.function = function findSpaceResorts (name, planet, searchCriteria, $vivContext) {
   let language = "en"
-  if ($vivContext.locale === "ko-KR") langugae = "ko"
+  if ($vivContext.locale === "ko-KR") language = "ko"
   return main.findSpaceResorts(name, planet, searchCriteria, language);
 }
